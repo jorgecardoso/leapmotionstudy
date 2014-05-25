a409ab510c9836aba2c5424d43d835b253090438
Jorge Cardoso <jorgecardoso@ieee.org>
2014-05-25 23:16:58 +0100

    Removed unused code.

--- RDataAnalysis/3.paths.R
+++ RDataAnalysis/3.paths.R
@@ -5,51 +5,6 @@ require(doBy)
 #library(reshape)
 #theme_set(theme_bw())
 
-# Multiple plot function
-#
-# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
-# - cols:   Number of columns in layout
-# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
-#
-# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
-# then plot 1 will go in the upper left, 2 will go in the upper right, and
-# 3 will go all the way across the bottom.
-#
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-    require(grid)
-    
-    # Make a list from the ... arguments and plotlist
-    plots <- c(list(...), plotlist)
-    
-    numPlots = length(plots)
-    
-    # If layout is NULL, then use 'cols' to determine layout
-    if (is.null(layout)) {
-        # Make the panel
-        # ncol: Number of columns of plots
-        # nrow: Number of rows needed, calculated from # of cols
-        layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                         ncol = cols, nrow = ceiling(numPlots/cols))
-    }
-    
-    if (numPlots==1) {
-        print(plots[[1]])
-        
-    } else {
-        # Set up the page
-        grid.newpage()
-        pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-        
-        # Make each plot, in the correct location
-        for (i in 1:numPlots) {
-            # Get the i,j matrix positions of the regions that contain this subplot
-            matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-            
-            print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                            layout.pos.col = matchidx$col))
-        }
-    }
-}
 
 
 ############################ data
@@ -79,7 +34,31 @@ print ( paste("X scale: ", minX, maxX, " Y scale: ", minY, maxY))
 
 
 
-
+# 
+# # plot the Speed
+# plot(dataTransformed[dataTransformed$Device=="Touchpad" &
+#                          dataTransformed$UserId==4 & 
+#                          dataTransformed$Block ==1 & 
+#                          dataTransformed$Sequence==1 & 
+#                          dataTransformed$CircleID==1,]$accels)
+# ggplot(dataTransformed[dataTransformed$Device=="Touchpad" &
+#                                 dataTransformed$UserId==4 & dataTransformed$Block ==1 & dataTransformed$Sequence==1 & dataTransformed$CircleID==1,] 
+#             ) +  geom_line(aes(y = speeds)) 
+#     coord_cartesian(xlim = c(minX, maxX), ylim=c(minY, maxY)) +
+#     facet_grid(Block ~ Sequence) +
+#     ylab("Block") +
+#     xlab("Sequence") +
+#     theme(legend.position="none") +
+#     ggtitle(paste("Device: ", device, " User: ", user))
+# p
+# 
+# p <- ggplot(dataTransformed, aes(x=Device, y=Throughput, group=Device, colour=Device )) + 
+#     stat_summary(fun.y="mean", geom="bar") + 
+#     stat_summary(fun.y="mean", geom="point", aes(shape=Device)) +
+#     theme() 
+# p
+# ggsave(file = "charts/Speed.pdf", width=20/2.54, height=16/2.54, dpi=100)
+# 
 
 
 # plot the paths for each user and device. a single plot aggregates one entire sequence
