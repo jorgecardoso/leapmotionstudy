3bf2bbe256581e520858d6e40d6d5c766049c699
Jorge Cardoso <jorgecardoso@ieee.org>
2014-06-09 13:52:28 +0100

    the same charts can now be created for different number of blocks.

--- RDataAnalysis/2.analysis.R
+++ RDataAnalysis/2.analysis.R
@@ -1,23 +1,30 @@
-#install.packages(c("ggplot2", "doBy"))
+#install.packages(c("ggplot2", "doBy", "psych"))
 require(ggplot2)
 require(doBy)
+require(psych)
 #library("stringr")
 #library(reshape)
 
 
 
 ############################ data
-dataMeasures <- read.csv(file="data/measures.txt", head=TRUE, sep="")
-
+data <- read.csv(file="data/measures.txt", head=TRUE, sep="")
 
 # convert the column to factor and name the levels
-dataMeasures$Device <- as.factor(dataMeasures$Device)
-levels(dataMeasures$Device) <- list( LeapMotion=c(0),  Mouse=c(1), Touchpad=c(2), LeapMotionTouchless=c(4))
+data$Device <- as.factor(data$Device)
+levels(data$Device) <- list( LeapMotion=c(0),  Mouse=c(1), Touchpad=c(2), LeapMotionTouchless=c(4))
 
 
+
+filenameprefix <- "all"
+dataMeasures <- data
 source("2.1.charts-per-device.R")
-
 source("2.2.charts-per-device+userid.R")
+source("2.3.anovas.R")
 
 
+filenameprefix <- "blocks4-8"
+dataMeasures <- data[data$Block > 3,]
+source("2.1.charts-per-device.R")
+source("2.2.charts-per-device+userid.R")
 source("2.3.anovas.R")
