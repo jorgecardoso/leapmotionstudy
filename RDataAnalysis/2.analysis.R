f83a3b97f7ae0a5cb89c2e6f6ba72a3698bf036c
Jorge Cardoso <jorgecardoso@ieee.org>
2014-06-12 17:31:30 +0100

    commented out the generation of plots per device+userid

--- RDataAnalysis/2.analysis.R
+++ RDataAnalysis/2.analysis.R
@@ -19,12 +19,12 @@ levels(data$Device) <- list( LeapMotion=c(0),  Mouse=c(1), Touchpad=c(2), LeapMo
 filenameprefix <- "all"
 dataMeasures <- data
 source("2.1.charts-per-device.R")
-source("2.2.charts-per-device+userid.R")
+#source("2.2.charts-per-device+userid.R")
 source("2.3.anovas.R")
 
 
 filenameprefix <- "blocks4-8"
 dataMeasures <- data[data$Block > 3,]
 source("2.1.charts-per-device.R")
-source("2.2.charts-per-device+userid.R")
+#source("2.2.charts-per-device+userid.R")
 source("2.3.anovas.R")
