e007cea7e70d2f51c690f75db5bd6d2387b41184
Jorge Cardoso <jorgecardoso@ieee.org>
2014-06-12 17:33:27 +0100

    added throughput variable to summary table and fixed generation of plots

--- RDataAnalysis/2.1.charts-per-device.R
+++ RDataAnalysis/2.1.charts-per-device.R
@@ -1,28 +1,33 @@
-
+source("functions.R")
 
 
 # print summary stats per Device
-s <- summaryBy(ErrorRate+TRE+TAC+MDC+ODC+MV+ME+MO+MovementTime~Device, 
+s <- summaryBy(ErrorRate+TRE+TAC+MDC+ODC+MV+ME+MO+MovementTime+Throughput~Device, 
                data=dataMeasures,
                FUN=c(mean,sd))
 s[,order(names(s))]
-write.table(s[,order(names(s))], file = paste("tables/", filenameprefix,"-measures-device-mean-sd.csv", sep=""), sep=",", row.names=FALSE)
+write.table(s[,order(names(s))], file = paste("tables/", filenameprefix,"-measures-device-summary.csv", sep=""), sep=",", row.names=FALSE)
 
 # describeby prints additional stats.
-# plbut it does not work well with write.table so we sink the output
-s1<-describeBy(dataMeasures[,6:22], dataMeasures$Device, mat=FALSE, digits=2)
-sink(file = paste("tables/", filenameprefix, "-measures-device.csv", sep=""), append = FALSE, type = c("output"),  split = FALSE)
-s1
-sink()
+# but it does not work well with write.table so we sink the output
+s1<-describeBy(dataMeasures[,c("ErrorRate","TRE","TAC","MDC","ODC","MV","ME","MO","MovementTime","Throughput")], dataMeasures$Device, mat=FALSE, digits=2)
+s1 <- do.call("rbind", s1)
+s1 <- cbind(Device=rownames(s1), s1)
+write.table(s1, file = paste("tables/", filenameprefix,"-measures-device-describeby.csv", sep=""), sep=",", row.names=FALSE)
+
+
 
 for (var in c("MovementTime", "ErrorRate", "TRE", "TAC", "MDC", "ODC", "MV", "ME", "MO", "Throughput") ) {
   
-  # plot the histograms of the movement time for each device
-  p <- ggplot(dataMeasures, aes_string(var, colour="Device" )) + 
+  # plot the histograms of the for each device
+  p <- ggplot(dataMeasures, aes_string(x=var, colour="Device" )) + 
     geom_histogram() + 
     #coord_flip() + 
     facet_wrap( ~ Device, ncol=1) +
     theme(legend.position="none") +
+    #ylab("Percentage (%)") +
+    #xlab("Sequence") +
+    #ggtitle(var) +
     theme()
   p
   ggsave(file = paste("charts/hist/",filenameprefix,"-",var,"-hist.pdf", sep=""), width=20/2.54, height=16/2.54, dpi=100)
@@ -30,10 +35,13 @@ for (var in c("MovementTime", "ErrorRate", "TRE", "TAC", "MDC", "ODC", "MV", "ME
   
   
   # plot the boxplots of the movement time for each device
-  p <- ggplot(dataMeasures, aes_string("Device", y=var,  colour="Device" )) + 
+  p <- ggplot(dataMeasures, aes_string(x="Device", y=var,  colour="Device" )) + 
     geom_boxplot() + 
     coord_flip() + 
     theme(legend.position="none") +
+    #ylab("Percentage (%)") +
+    #xlab("Sequence") +
+    #ggtitle(var) +
     theme()
   p
   ggsave(file = paste("charts/boxplot/",filenameprefix,"-",var,"-boxplot.pdf", sep=""), width=20/2.54, height=16/2.54, dpi=100)
@@ -45,10 +53,12 @@ for (var in c("MovementTime", "ErrorRate", "TRE", "TAC", "MDC", "ODC", "MV", "ME
     stat_summary(fun.y="mean", geom="line") + 
     stat_summary(fun.y="mean", geom="point", aes(shape=Device)) + 
     theme(legend.direction = "horizontal", legend.position = "top") +
+    #ylab("Percentage (%)") +
+    #xlab("Sequence") +
+    #ggtitle(var) +
     theme() #noop
   p
   ggsave(file = paste("charts/lineplot/",filenameprefix,"-",var,"-block-lineplot.pdf", sep=""), width=20/2.54, height=16/2.54, dpi=100)
-  
 }
 
 
@@ -65,10 +75,43 @@ ggsave(file = paste("charts/",filenameprefix,"-MO-TRE.pdf", sep=""), width=20/2.
 
 
 
-# plot the throughput
-p <- ggplot(dataMeasures, aes(x=Device,y=Throughput, group=Device, colour=Device, fill=Device )) + 
-  stat_summary(fun.y="mean", geom="bar") + 
-  stat_summary(fun.y="mean", geom="point", aes(shape=Device)) +
+# plot the throughput and ErrorRate
+
+describe <- describeBy(dataMeasures$Throughput, list(dataMeasures$Device))
+describe<-do.call("rbind",describe)
+describe <- cbind(Device=rownames(describe), describe)
+levels(describe$Device) <- list( Mouse=c("Mouse"), Touchpad=c("Touchpad"), LeapMotionTouchless=c("LeapMotionTouchless"))
+p1 <- ggplot(describe, aes(x=Device, y=mean, colour=Device, fill=Device )) + 
+  geom_bar(stat="identity") +
+  ylab("BPS") +
+  #xlab("Sequence") +
+  ggtitle("Throughput") +
+  geom_errorbar(aes(ymin=mean-1.96*se, ymax=mean+1.96*se), colour="Black",
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9)) +
+    theme(legend.position="none") +
   theme() 
-p
-ggsave(file = paste("charts/",filenameprefix,"-Throughput.pdf",sep=""), width=20/2.54, height=16/2.54, dpi=100)
+p1
+#ggsave(file = paste("charts/",filenameprefix,"-Throughput.pdf",sep=""), width=20/2.54, height=16/2.54, dpi=100)
+
+describe <- describeBy(dataMeasures$ErrorRate, list(dataMeasures$Device))
+describe<-do.call("rbind",describe)
+describe <- cbind(Device=rownames(describe), describe)
+levels(describe$Device) <- list( Mouse=c("Mouse"), Touchpad=c("Touchpad"), LeapMotionTouchless=c("LeapMotionTouchless"))
+
+p2 <- ggplot(describe, aes(x=Device, y=mean*100, colour=Device, fill=Device )) + 
+  geom_bar(stat="identity") +
+  ylab("Percentage (%)") +
+  #xlab("Sequence") +
+  ggtitle("ErrorRate") +
+  geom_errorbar(aes(ymin=100*(mean-1.96*se), ymax=100*(mean+1.96*se)), colour="Black",
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9)) +
+  
+    theme(legend.position=c(0,0.9), legend.direction="horizontal") +
+  theme() 
+p2
+
+pdf(file = paste("charts/",filenameprefix,"-Throughput+ErrorRate.pdf",sep=""), width=20/2.54, height=16/2.54) 
+multiplot(p1, p2, cols=2)
+dev.off()
