b5a620af62315e2129321a14ca093f19ace54109
Jorge Cardoso <jorgecardoso@ieee.org>
2014-06-12 17:33:46 +0100

    added misc temporary code

--- RDataAnalysis/2.3.anovas.R
+++ RDataAnalysis/2.3.anovas.R
@@ -1,4 +1,22 @@
+#install.packages("ppcor")
+require(ppcor)
 
+dataMeasures$DeviceNumber <- as.numeric(dataMeasures$Device)
+
+View(dataMeasures)
+
+cors <- cor(dataMeasures[,c(2,6:13, 18, 22)])
+View(cors)
+
+class(dataMeasures$Throughput)
+class(dataMeasures$TRE)
+
+pcor(dataMeasures[,c(6:13, 18, 22)])
+
+pcor.test(dataMeasures$Throughput,dataMeasures$MovementTime,dataMeasures[,c("DeviceNumber", "UserId")])
+
+#cors.test <- cor.test(~MO+Throughput,data=dataMeasures)
+#cors.test
 
 # analysis of variance
 bartlett.test(MovementTime~Device,data=dataMeasures)
@@ -47,7 +65,8 @@ summary(a)
 
 
 
-
+a <- aov(Throughput~Block,data=dataMeasures[dataMeasures$Block>4 & dataMeasures$Device=="Touchpad",])
+summary(a)
 
 # analysis of variance
 bartlett.test(MovementTime~Device,data=dataMeasures)
