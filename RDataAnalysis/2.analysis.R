1954085c1b5913fc00f870a17037389ba4f898be
Jorge Cardoso <jorgecardoso@ieee.org>
2014-06-13 10:19:08 +0100

    Added initial learning effect testing

--- RDataAnalysis/2.analysis.R
+++ RDataAnalysis/2.analysis.R
@@ -28,3 +28,29 @@ dataMeasures <- data[data$Block > 3,]
 source("2.1.charts-per-device.R")
 #source("2.2.charts-per-device+userid.R")
 source("2.3.anovas.R")
+
+
+my.helmert = matrix(c(7/8, -1/8, -1/8, -1/8, -1/8, -1/8, -1/8, -1/8,
+                       0,  6/7, -1/7, -1/7, -1/7, -1/7, -1/7, -1/7,
+                       0,    0,  5/6, -1/6, -1/6, -1/6, -1/6, -1/6,
+                       0,    0,    0,  4/5, -1/5, -1/5, -1/5, -1/5,
+                       0,    0,    0,    0,  3/4, -1/4, -1/4, -1/4, 
+                       0,    0,    0,    0,    0,  2/3, -1/3, -1/3, 
+                       0,    0,    0,    0,    0,    0,  1/2, -1/2 
+                     ), ncol = 7)
+
+my.rev.helmert = matrix(c(-1/2,  1/2,    0,    0,   0,    0,   0,  0,  
+                     -1/3, -1/3,  3/3,    0,   0,    0,   0,  0,
+                     -1/4, -1/4, -1/4,  3/4,   0,    0,   0,  0,
+                     -1/5, -1/5, -1/5, -1/5, 4/5,    0,   0,  0,
+                     -1/6, -1/6, -1/6, -1/6, -1/6, 5/6,   0,  0, 
+                     -1/7, -1/7, -1/7, -1/7, -1/7, -1/7, 6/7, 0,
+                     -1/8, -1/8, -1/8, -1/8, -1/8, -1/8, -1/8,7/8
+                     
+), ncol = 7)
+
+contrasts(dataMeasures$Block) = my.helmert
+summary(lm(MovementTime~Block, data=dataMeasures[dataMeasures$Device=="LeapMotionTouchless",]))
+summary(lm(MovementTime~Block, data=dataMeasures[dataMeasures$Device=="Touchpad",]))
+summary(lm(MovementTime~Block, data=dataMeasures[dataMeasures$Device=="Mouse",]))
+summary(lm(MovementTime~Block, data=dataMeasures))
