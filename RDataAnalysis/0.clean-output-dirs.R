f4de4ac7f5363ebddfba8283c250d69254d89ee1
Jorge Cardoso <jorgecardoso@ieee.org>
2014-07-17 13:54:46 +0100

    Removed R scripts from this source repository.

--- RDataAnalysis/0.clean-output-dirs.R
+++
@@ -1,7 +0,0 @@
-# Cleans the output directories for charts and tables
-# Does not delete the transformed.txt and measures.txt data files
-
-unlink("charts/*.pdf", recursive=TRUE)
-unlink("charts/*/*.pdf", recursive=TRUE)
-
-unlink("tables/*.csv", recursive=TRUE)
